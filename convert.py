17950e9ecc470cd6bbb967cd9acc7e29e7895a9b
Aaron Erlich <aaron.erlich@gmail.com>
2015-08-10 09:47:42 -0700

    First version that got the web app running

--- convert.py
+++ convert.py
@@ -1,19 +1,22 @@
 import csv
 import sys
 import sqlite3
+
 '''
 This little piece of code will port a csv exports from mongoexport and input it into a sqlite3 database
 could be abstracted to work generically, must use python 3+ for UTF reasons. 
 '''
-conn = sqlite3.connect("saNews.db")
+
+conn = sqlite3.connect("db.sqlite3")
 
 c = conn.cursor()
 
-try:
-    c.execute('drop table saNews')
-except:
-    pass
-c.execute('create table saNews (_id text, author text ,downloaded_at text,id text,meta_description text, owner text,publication text,published text,sub_type text,summary text,text text,title text,url text);')
+# try:
+#     c.execute('drop table saNews')
+# except:
+#     pass
+
+# c.execute('create table saNews (_id text, author text ,downloaded_at text,id text,meta_description text, owner text,publication text,published text,sub_type text,summary text,text text,title text,url text);')
 
 csv.field_size_limit(sys.maxsize)
 
@@ -22,7 +25,21 @@ with open("saNewsDb.csv", "r") as f:
     n = 0
     for row in csvReader:
         if n != 0:
-            c.execute('INSERT INTO saNews VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)', row)
+            blarg = row[0:3]
+            blarg += row[4:13]
+            for i in range(0,12):
+                if blarg[i] is None:
+                    blarg[i] = ''
+            try:
+                c.execute('INSERT INTO newscoder_story ' \
+                          '(_id, author, downloaded_at, ' \
+                          'meta_description, owner, publication, ' \
+                          'published, sub_type, summary, text, title, url)' \
+                          ' VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', blarg)
+            except sqlite3.IntegrityError as e:
+                print(blarg)
+                print(e)
+                      
         n += 1
 
 conn.commit()
--- newscoder/models.py
+++ newscoder/models.py
@@ -2,6 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+
 class Story(models.Model):
-    story_text = models.CharField(max_length=2000)
-    coder_code = models.IntegerField(default=-10)
+    '''This class represents a story and the coding of story xxx'''
+    author = models.CharField(max_length=2000)
+    downloaded_at = models.CharField(max_length=2000)
+    _id = models.CharField(max_length=2000, primary_key=True)
+    meta_description = models.CharField(max_length=2000)
+    owner = models.CharField(max_length=2000)
+    publication = models.CharField(max_length=2000)
+    published = models.CharField(max_length=2000)
+    sub_type = models.CharField(max_length=2000)
+    summary = models.CharField(max_length=2000)
+    text = models.CharField(max_length=2000, blank=True, default='')
+    title = models.CharField(max_length=2000)
+    url = models.CharField(max_length=2000)
+
+class Coder(models.Model):
+    coder_name = models.CharField(max_length=100)
+    coder_id = models.IntegerField()
+
+
+class StoryCoding(models.Model):
+    story_id = models.IntegerField(default=-10)
+    coder_id = models.IntegerField()
+    coder_coding = models.IntegerField() #control
+
+    
